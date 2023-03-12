814771886e532280287ee98b89bbdc1a033d2cde
Muhammed Kalender <muhammed@siyah.dev>
2023-03-12 22:58:50 +0300

    Init spring boot

---
+++ src/main/java/dev/siyah/filemanager/FilerecordApplication.java
@@ -0,0 +1,14 @@
+package dev.siyah.filemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilerecordApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/dev/siyah/filemanager/FilerecordApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.siyah.filemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilerecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
